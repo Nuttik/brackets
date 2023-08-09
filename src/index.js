f14991ed36176cc2f2e960d40dd99129d9bace5a
Natalia Repkina <pn.88@mail.ru>
2023-08-09 17:23:03 +0300

    init: start solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.map((item) => {
+      if (!item.includes(str[i])) {
+        return false;
+      } else {
+        let index = item.indexOf(str[i]);
+        if (index == 0) {
+          result.push(str[i]);
+        } else {
+          if (result[result.length - 1] == item[0]) {
+            result.pop();
+          }
+        }
+      }
+    });
+  }
+  if (result.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
