9daf61a8041e38caf2a4056b27dc2f6acefd905f
Natalia Repkina <pn.88@mail.ru>
2023-08-09 17:28:20 +0300

    fix: solution

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ module.exports = function check(str, bracketsConfig) {
     bracketsConfig.map((item) => {
       if (!item.includes(str[i])) {
         return false;
+      } else if ((item[0] = item[i])) {
+        if (result[result.length - 1] == item[0]) {
+          result.pop();
+        } else {
+          result.push(str[i]);
+        }
       } else {
         let index = item.indexOf(str[i]);
+
         if (index == 0) {
           result.push(str[i]);
         } else {
