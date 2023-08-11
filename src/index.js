d52f3af77eec0e1d7181cb0c49e0992f00b4700b
Natalia Repkina <pn.88@mail.ru>
2023-08-11 14:42:00 +0300

    fix: logic solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   let result = [];
+
   if (str.length % 2 !== 0) return false;
 
   for (let i = 0; i < str.length; i++) {
     bracketsConfig.forEach((item) => {
-      if (!item.includes(str[i])) {
-      } else if (item[0] == item[1]) {
+      if (item[0] == item[1] && item.includes(str[i])) {
         if (result[result.length - 1] == item[0]) {
           result.pop();
         } else {
           result.push(str[i]);
         }
-      } else {
+      } else if (item.includes(str[i])) {
         let index = item.indexOf(str[i]);
 
         if (index == 0) {
